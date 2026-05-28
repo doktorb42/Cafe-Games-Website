a29342ae78c185538055894cc673367de56c6313
doktorb42 <bogdandoktorek@gmail.com>
2026-05-28 19:24:42 +0200

    new

--- start-tomcat-debug.bat
+++ start-tomcat-debug.bat
@@ -1,9 +1,14 @@
 @echo off
+setlocal
+
+set "PROJECT_DIR=%~dp0"
+set "TOMCAT_HOME=D:\java-projekts\apache-tomcat"
+
 echo Killing old Tomcat/Java processes...
 for /f "tokens=5" %%a in ('netstat -ano ^| findstr :8080') do taskkill /F /PID %%a >nul 2>nul
 for /f "tokens=5" %%a in ('netstat -ano ^| findstr :8000') do taskkill /F /PID %%a >nul 2>nul
 
-timeout /t 1 >nul
+ping 127.0.0.1 -n 2 >nul
 
 echo Forcing JDK17...
 set "JAVA_HOME=C:\Program Files\Eclipse Adoptium\jdk-17.0.17.10-hotspot"
@@ -15,9 +20,20 @@ call mvn clean compile war:exploded -DskipTests
 
 IF %ERRORLEVEL% NEQ 0 (
    echo BUILD FAILED - TOMCAT NOT STARTED
+   echo If this is the first run, verify Maven can access https://repo.maven.apache.org
+   exit /b %ERRORLEVEL%
+)
+
+echo Deploying ROOT app to Tomcat...
+if exist "%TOMCAT_HOME%\webapps\ROOT" rmdir /s /q "%TOMCAT_HOME%\webapps\ROOT"
+mkdir "%TOMCAT_HOME%\webapps\ROOT" >nul 2>nul
+xcopy "%PROJECT_DIR%target\ROOT\*" "%TOMCAT_HOME%\webapps\ROOT\" /e /i /y >nul
+
+IF %ERRORLEVEL% GEQ 4 (
+   echo DEPLOY FAILED - FILE COPY ERROR
    exit /b %ERRORLEVEL%
 )
 
 echo Starting Tomcat JPDA...
-cd /d D:\java-projekts\apache-tomcat\bin
+cd /d "%TOMCAT_HOME%\bin"
 call catalina.bat jpda run
